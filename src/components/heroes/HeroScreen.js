9685d96710722761c15a81cc68a48175ee1d082e
alejoboga <alejoboga20@gmail.com>
2021-04-20 09:36:45 -0300

    styling hero screen

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import { getHeroesById } from '../../selectors/getHeroesById';
 
-const HeroScreen = () => {
+const HeroScreen = ({ history }) => {
   const { heroId } = useParams();
   const hero = getHeroesById(heroId);
 
   if (!hero) {
     return <Redirect to='/' />;
   }
+
+  const handleReturn = () => {
+    if (history.length <= 2) {
+      history.push('/');
+    }
+
+    history.goBack();
+  };
 
   const {
     superhero,
@@ -19,8 +27,38 @@ const HeroScreen = () => {
   } = hero;
 
   return (
-    <div>
-      <h1>Hero Screen</h1>
+    <div className='row mt-5'>
+      <div className='col-4'>
+        <img
+          src={`../assets/heroes/${heroId}.jpg`}
+          className='img-thumbnail'
+          alt={superhero}
+        />
+      </div>
+      <div className='col-8'>
+        <h3>{superhero}</h3>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>
+            <b>Alter Ego: </b>
+            {alter_ego}
+          </li>
+          <li className='list-group-item'>
+            <b>Publisher: </b>
+            {publisher}
+          </li>
+          <li className='list-group-item'>
+            <b>First Appearance: </b>
+            {first_appearance}
+          </li>
+        </ul>
+
+        <h5>Characters</h5>
+        <p>{characters}</p>
+
+        <button className='btn btn-outline-info' onClick={handleReturn}>
+          Return
+        </button>
+      </div>
     </div>
   );
 };
