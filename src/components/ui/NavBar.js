5d1f6722349ab50c95652b4d83513f46e2645d6a
alejoboga <alejoboga20@gmail.com>
2021-04-22 09:30:30 -0300

    Building logout and adding useHistory

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,9 +1,16 @@
 import React, { useContext } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useHistory } from 'react-router-dom';
 import { AuthContext } from '../auth/AuthContext';
+import { types } from '../types/types';
 
 const Navbar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, dispatch } = useContext(AuthContext);
+  const history = useHistory();
+
+  const handleLogout = () => {
+    history.replace('/login');
+    dispatch({ type: types.logout });
+  };
 
   return (
     <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
@@ -44,14 +51,9 @@ const Navbar = () => {
       <div className='navbar-collapse collapse w-100 order-3 dual-collapse2'>
         <ul className='navbar-nav ml-auto'>
           <span className='nav-item nav-link'>{user.name}</span>
-          <NavLink
-            activeClassName='active'
-            className='nav-item nav-link'
-            exact
-            to='/login'
-          >
+          <button className='nav-item nav-link btn' onClick={handleLogout}>
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
