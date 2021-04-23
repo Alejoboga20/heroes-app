c6d75b64b80a2c73e7cdaa0dbd8491e41ecc754d
alejoboga <alejoboga20@gmail.com>
2021-04-23 15:48:46 -0300

    Tests for DashboardRoutes

---
+++ src/tests/routers/DashboardRoutes.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { AuthContext } from '../../components/auth/AuthContext';
+import DashboardRoutes from '../../routers/DashboardRoutes';
+import { MemoryRouter } from 'react-router';
+
+describe('DashboardRoutes tests', () => {
+  const contextValue = {
+    dispatch: jest.fn(),
+    user: { logged: true, name: 'Test' }
+  };
+
+  test('should display properly', () => {
+    const wrapper = mount(
+      <AuthContext.Provider value={contextValue}>
+        <MemoryRouter>
+          <DashboardRoutes />
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('span').text().trim()).toBe('Test');
+  });
+});
