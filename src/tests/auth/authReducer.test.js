d22f553d3033a6369b432af6c133eae0cc1d9a4a
alejoboga <alejoboga20@gmail.com>
2021-04-23 09:29:16 -0300

    Tests for reducer

---
+++ src/tests/auth/authReducer.test.js
@@ -0,0 +1,31 @@
+import { authReducer } from '../../components/auth/authReducer';
+import { types } from '../../components/types/types';
+
+describe('authReducer Tests', () => {
+  const userName = 'Test';
+  const initialState = {};
+
+  const loginAction = {
+    type: types.login,
+    payload: {
+      userName
+    }
+  };
+
+  const logoutAction = { type: types.logout };
+
+  test('should return default state', () => {
+    const defaultState = authReducer(initialState, {});
+    expect(defaultState).toEqual(initialState);
+  });
+
+  test('should authenticate and put userName', () => {
+    const authenticatedUser = authReducer(initialState, loginAction);
+    expect(authenticatedUser).toEqual({ userName, logged: true });
+  });
+
+  test('should remove userName and log out', () => {
+    const notAuthenticatedUser = authReducer(initialState, logoutAction);
+    expect(notAuthenticatedUser).toEqual({ logged: false });
+  });
+});
