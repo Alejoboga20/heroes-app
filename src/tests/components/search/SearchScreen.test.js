eeea119d6de4e6090cb07d490725e24a1c63ae7b
alejoboga <alejoboga20@gmail.com>
2021-04-27 09:37:40 -0300

    More tests for search screen

--- src/tests/components/search/SearchScreen.test.js
+++ src/tests/components/search/SearchScreen.test.js
@@ -4,6 +4,12 @@ import { MemoryRouter, Route } from 'react-router';
 import SearchScreen from '../../../components/search/SearchScreen';
 
 describe('SearchScreen Tests', () => {
+  const historyMock = {
+    push: jest.fn()
+  };
+
+  const q = 'noHeroName';
+
   test('should display properly with default values', () => {
     const wrapper = mount(
       <MemoryRouter initialEntries={['/search']}>
@@ -22,5 +28,39 @@ describe('SearchScreen Tests', () => {
     );
 
     expect(wrapper.find('input').prop('value')).toBe('batman');
+  });
+
+  test('should show an error if no hero is found', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[`/search?q=${q}`]}>
+        <Route path='/search' component={SearchScreen} />
+      </MemoryRouter>
+    );
+
+    expect(wrapper.find('.alert-danger').text()).toBe(
+      `There is no hero with ${q}`
+    );
+  });
+
+  test('should call history push', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/search']}>
+        <Route
+          path='/search'
+          component={() => <SearchScreen history={historyMock} />}
+        />
+      </MemoryRouter>
+    );
+
+    wrapper.find('input').simulate('change', {
+      target: {
+        name: 'searchText',
+        value: 'batman'
+      }
+    });
+
+    wrapper.find('form').prop('onSubmit')({ preventDefault() {} });
+
+    expect(historyMock.push).toHaveBeenCalledWith('?q=batman');
   });
 });
